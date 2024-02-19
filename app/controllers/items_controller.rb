36b0e2dc20648debb7d4fa32767264869872affe
satumaimo77 <lucky.no0077@gmail.com>
2024-02-19 16:57:18 +0900

    item コントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,63 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_sign_in, except: [:index, :show]
   def index
+    @items = Item.includes(:user).order('created_at DESC')
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+    return unless current_user.id != @item.user_id
+
+    redirect_to root_path
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    if current_user.id == item.user_id
+      item.destroy
+      redirect_to root_path
+    else
+      redirect_to item_path(item.id)
+    end
   end
 
   private
+
+  def item_params
+    params.require(:item).permit(:name, :info, :post_data, :grade_id, :category_id).merge(user_id: current_user.id)
+  end
 
   def move_to_sign_in
     unless user_signed_in?
       redirect_to new_user_session_path
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
